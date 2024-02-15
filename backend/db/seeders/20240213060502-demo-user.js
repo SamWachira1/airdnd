df854e510937f57d8a663c80fc82be7111e1dd0e
Samuel <187swachira@gmail.com>
2024-02-15 02:44:40 -0500

    updating files

--- backend/db/seeders/20240213060502-demo-user.js
+++ backend/db/seeders/20240213060502-demo-user.js
@@ -13,23 +13,23 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await User.bulkCreate([
       {
-        email: 'demo@user.io',
         firstName: 'demofirstName0',
         lastName: 'demolastName0',
+        email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
-        email: 'user1@user.io',
         firstName: 'demofirstName1',
         lastName: 'demolastName1',
+        email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
-        email: 'user2@user.io',
         firstName: 'demofirstName2',
         lastName: 'demolastName2',
+        email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
       }
