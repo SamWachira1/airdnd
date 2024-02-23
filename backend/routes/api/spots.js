ed08e542a3bf8e178799250a97c12e0bb1c0d297
Samuel <187swachira@gmail.com>
2024-02-23 17:14:27 -0500

    fixing production errors

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -67,7 +67,7 @@ router.post("/:spotId/images", requireAuth, async (req, res) => {
     const { spotId } = req.params;
     const { url, preview } = req.body;
 
-    
+
 
     const spot = await Spot.findOne({ where: { id: spotId } });
 
@@ -83,7 +83,7 @@ router.post("/:spotId/images", requireAuth, async (req, res) => {
             imageableType: 'Spot',
         });
 
-      
+
         const safeResponse = {
             url: newImage.url,
             preview: newImage.preview
@@ -91,8 +91,8 @@ router.post("/:spotId/images", requireAuth, async (req, res) => {
 
 
         res.status(200).json(safeResponse)
-    }else {
-        res.status(403).json({message:  "Forbidden"})
+    } else {
+        res.status(403).json({ message: "Forbidden" })
     }
 
 });
@@ -100,7 +100,7 @@ router.post("/:spotId/images", requireAuth, async (req, res) => {
 
 router.put('/:spotId', requireAuth, validateSpot, async (req, res) => {
 
-    const currUser =  req.user;
+    const currUser = req.user;
     const { spotId } = req.params
     const { address, city, state, country, lat, lng, name, description, price } = req.body
 
@@ -143,54 +143,76 @@ router.put('/:spotId', requireAuth, validateSpot, async (req, res) => {
 
         res.status(200).json(responseEdit)
 
-    }else {
+    } else {
         return res.status(404).json({ message: "Spot couldn't be found" });
 
     }
 
-
-
 })
 
 router.get('/current', requireAuth, async (req, res) => {
-    // console.log("\n\n\n",req.user , "\n\n\n")
-    // console.log("\n\n\n",req.user , "\n\n\n")
+
     let currentUser = req.user
-    // console.log("\n\n\n",currentUser , "\n\n\n")
 
-    // console.log(req.user)
-
-    let getUserSpots = await Spot.findAll({
+    let getAllSpots = await Spot.findAll({
 
         where: { ownerId: currentUser.id },
 
     })
 
-    if (!getUserSpots) {
-        return res.status(403).json({ message: "Fobidden" });
-    }
+    const formattedSpots = [];
 
-    const formattedSpots = getUserSpots.map((spot) => {
+    for (const spot of getAllSpots) {
+
+        const reviews = await Review.findAll({
+            where: { spotId: spot.id },
+            attributes: ['stars'],
+
+
+        });
+
+        const totalStars = reviews.reduce((sum, review) => sum + review.stars, 0);
+        const avgRating = reviews.length > 0 ? totalStars / reviews.length : 0;
+
         let createdAtDate = new Date(spot.createdAt);
         let upadatedAtDate = new Date(spot.updatedAt)
 
         createdAtDate = createdAtDate.toISOString().replace('T', ' ').split('.')[0];
         upadatedAtDate = upadatedAtDate.toISOString().replace('T', ' ').split('.')[0];
 
-        return {
-            ...spot.toJSON(),
+        const spotImages = await Image.findAll({
+            attributes: ['id', 'url'],
+            where: { imageableType: 'Spot', imageableId: spot.id },
+        });
+
+
+        const formattedSpot = {
+            id: spot.id,
+            ownerId: spot.id,
+            address: spot.address,
+            city: spot.city,
+            state: spot.state,
+            country: spot.country,
+            lat: spot.lat,
+            lng: spot.lng,
+            name: spot.name,
+            description: spot.description,
+            price: spot.price,
             createdAt: createdAtDate,
             updatedAt: upadatedAtDate,
+            avgRating: avgRating,
+            previewImage: spotImages.length > 0 ? spotImages[0].url : null,
         };
-    });
 
+        formattedSpots.push(formattedSpot);
+    }
 
-    let response = {
+    const response = {
         Spots: formattedSpots,
     };
 
 
-    res.json(response)
+    res.status(200).json(response);
 
 
 
@@ -250,7 +272,7 @@ router.get('/:id', async (req, res) => {
 })
 
 router.delete('/:spotId', requireAuth, async (req, res) => {
-    let currUser = req.user 
+    let currUser = req.user
     let { spotId } = req.params
 
     let spot = await Spot.findOne({ where: { id: spotId } })
@@ -259,14 +281,14 @@ router.delete('/:spotId', requireAuth, async (req, res) => {
         return res.status(404).json({ message: "Spot couldn't be found" });
     }
 
-    if (currUser.id === spot.ownerId){
-        
-            await spot.destroy()
-        
-            res.status(200).json({ message: "Successfully deleted" })
+    if (currUser.id === spot.ownerId) {
 
-    }else{
-        res.status(403).json({message: "Forbidden"})
+        await spot.destroy()
+
+        res.status(200).json({ message: "Successfully deleted" })
+
+    } else {
+        res.status(403).json({ message: "Forbidden" })
     }
 
 })
@@ -275,106 +297,67 @@ router.delete('/:spotId', requireAuth, async (req, res) => {
 
 router.get('/', async (req, res) => {
 
-    // let getAllSpots = await Spot.findAll({
-    //     attributes: [
-    //         'id',
-    //         'ownerId',
-    //         "address",
-    //         "city",
-    //         "state",
-    //         "country",
-    //         "lat",
-    //         "lng",
-    //         "name",
-    //         "description",
-    //         "price",
-    //         "createdAt",
-    //         "updatedAt",
-
-    //         [Sequelize.fn('avg', Sequelize.col('Reviews.stars')), 'avgRating']
-    //     ],
-    //     include: [
-    //         {
-    //             model: Review,
-    //             attributes: [],
-    //             // where: {  spotId: Sequelize.col('Spot.id')}, // Join based on spotId
-    //         },
-    //         {
-    //             model: Image,
-    //             attributes: ['id', 'url'],
-    //             as: 'SpotImages', // Assuming your association alias is 'SpotImages'
-    //             where: { imageableType: 'Spot' }, // Filter images by Spot
-    //         },
-
-          
-    //     ],
-
-    //     group: ['SpotImages.id']
-        
-    // })
 
 
     const getAllSpots = await Spot.findAll();
 
-            const formattedSpots = [];
+    const formattedSpots = [];
 
-            for (const spot of getAllSpots) {
+    for (const spot of getAllSpots) {
 
-                const reviews = await Review.findAll({
-                    where: { spotId: spot.id },
-                    attributes: ['stars'],
-
-
-                });
-
-            const totalStars = reviews.reduce((sum, review) => sum + review.stars, 0);
-            const avgRating = reviews.length > 0 ? totalStars / reviews.length : 0;
-
-            let createdAtDate = new Date(spot.createdAt);
-            let upadatedAtDate = new Date(spot.updatedAt)
-        
-            createdAtDate = createdAtDate.toISOString().replace('T', ' ').split('.')[0];
-            upadatedAtDate = upadatedAtDate.toISOString().replace('T', ' ').split('.')[0];
-
-            const spotImages = await Image.findAll({
-                attributes: ['id', 'url'],
-                where: { imageableType: 'Spot', imageableId: spot.id },
-            });
+        const reviews = await Review.findAll({
+            where: { spotId: spot.id },
+            attributes: ['stars'],
 
 
-            const formattedSpot = {
-           
-                id: spot.id,
-                ownerId: spot.id,
-                address: spot.address,
-                city: spot.city,
-                state: spot.state,
-                country: spot.country,
-                lat: spot.lat,
-                lng: spot.lng,
-                name: spot.name,
-                description: spot.description,
-                price: spot.price,
-                createdAt: createdAtDate,
-                updatedAt: upadatedAtDate,
-                avgRating: avgRating,
-                previewImage: spotImages.length > 0 ? spotImages[0].url : null,
-            };
+        });
 
-            formattedSpots.push(formattedSpot);
-        }
+        const totalStars = reviews.reduce((sum, review) => sum + review.stars, 0);
+        const avgRating = reviews.length > 0 ? totalStars / reviews.length : 0;
 
-            const response = {
-            Spots: formattedSpots,
-            };
+        let createdAtDate = new Date(spot.createdAt);
+        let upadatedAtDate = new Date(spot.updatedAt)
+
+        createdAtDate = createdAtDate.toISOString().replace('T', ' ').split('.')[0];
+        upadatedAtDate = upadatedAtDate.toISOString().replace('T', ' ').split('.')[0];
+
+        const spotImages = await Image.findAll({
+            attributes: ['id', 'url'],
+            where: { imageableType: 'Spot', imageableId: spot.id },
+        });
 
 
-        res.status(200).json(response);
+        const formattedSpot = {
+
+            id: spot.id,
+            ownerId: spot.id,
+            address: spot.address,
+            city: spot.city,
+            state: spot.state,
+            country: spot.country,
+            lat: spot.lat,
+            lng: spot.lng,
+            name: spot.name,
+            description: spot.description,
+            price: spot.price,
+            createdAt: createdAtDate,
+            updatedAt: upadatedAtDate,
+            avgRating: avgRating,
+            previewImage: spotImages.length > 0 ? spotImages[0].url : null,
+        };
+
+        formattedSpots.push(formattedSpot);
+    }
+
+    const response = {
+        Spots: formattedSpots,
+    };
+
+
+    res.status(200).json(response);
 
 
 })
-
-
 
 
 router.post('/', requireAuth, validateSpot, async (req, res) => {
@@ -415,8 +398,8 @@ router.post('/', requireAuth, validateSpot, async (req, res) => {
         }
 
         res.status(201).json(safeSpot)
-    }else {
-        res.status(403).json({message: "Forbidden"})
+    } else {
+        res.status(403).json({ message: "Forbidden" })
     }
 
 
