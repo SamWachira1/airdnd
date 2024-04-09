f39d2589cb80ff50fbb9910cea249e105d93f0a1
Samuel <187swachira@gmail.com>
2024-04-09 02:30:35 -0400

    added OpenModalMenuItem

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,54 +1,59 @@
-import { NavLink } from "react-router-dom"
-import { useSelector } from "react-redux"
+// import { NavLink } from "react-router-dom"
+// import { useSelector } from "react-redux"
 // import { logoutThunk } from "../../store/session"
-import ProfileButton from "./ProfileButton"
-import { FaUserCircle } from "react-icons/fa";
+// import ProfileButton from "./ProfileButton"
+// import { FaUserCircle } from "react-icons/fa";
+// import { useState } from "react";
+// import OpenModalButton from '../OpenModalButton/OpenModalButton';
+// import LoginFormModal from '../ LoginFormModal/LoginFormModal';
+// import SignupFormModal from "../SignUpFormModal/SignupFormModal";
+
+
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
 import styles from './Navigation.module.css';
-import { useState } from "react";
 import logo from '../../../public/images/stayScapelogo.png'
-import OpenModalButton from '../OpenModalButton/OpenModalButton';
-import LoginFormModal from '../ LoginFormModal/LoginFormModal';
-import SignupFormModal from "../SignUpFormModal/SignupFormModal";
+
+
 
 const Navigation = ({ isLoaded }) => {
-    const [showMenu, setShowMenu] = useState(false)
     const sessionUser = useSelector(state => state.session.user)
 
+    // const sessionLinks = sessionUser ? (
+    //     <>
+    //         <li>
+    //             <ProfileButton user={sessionUser} />
+    //         </li>
 
-    const sessionLinks = sessionUser ? (
-        <>
-            <li>
-                <ProfileButton user={sessionUser} />
-            </li>
+    //     </>
+    // ) : (
+    //     <>
+    //         <div className={styles.profileButtonContainer}>
+    //             <button className={styles.profileButton} onClick={() => setShowMenu(!showMenu)}>
+    //                 <FaUserCircle size={'2em'} />
+    //             </button>
+    //             <ul className={showMenu ? styles.profileDropdown : styles.hidden}>
+    //                 <li>
+    //                     <OpenModalButton
+    //                         buttonText="Log In"
+    //                         modalComponent={<LoginFormModal />}
+    //                     />
+    //                 </li>
+    //                 <li>
+    //                      <OpenModalButton
+    //                         buttonText="Sign Up"
+    //                         modalComponent={<SignupFormModal />}
+    //                     />
+    //                 </li>
+    //             </ul>
 
-        </>
-    ) : (
-        <>
-            <div className={styles.profileButtonContainer}>
-                <button className={styles.profileButton} onClick={() => setShowMenu(!showMenu)}>
-                    <FaUserCircle size={'2em'} />
-                </button>
-                <ul className={showMenu ? styles.profileDropdown : styles.hidden}>
-                    <li>
-                        <OpenModalButton
-                            buttonText="Log In"
-                            modalComponent={<LoginFormModal />}
-                        />
-                    </li>
-                    <li>
-                         <OpenModalButton
-                            buttonText="Sign Up"
-                            modalComponent={<SignupFormModal />}
-                        />
-                    </li>
-                </ul>
-
-            </div>
+    //         </div>
 
 
-        </>
+    //     </>
 
-    )
+    // )
 
     return (
 
@@ -58,7 +63,11 @@ const Navigation = ({ isLoaded }) => {
                     <img src={logo} alt="Logo" />
                 </NavLink>
             </li>
-            {isLoaded && sessionLinks}
+            {isLoaded && (
+                <li>
+                    <ProfileButton user={sessionUser} />
+                </li>
+            )}
         </ul>
 
 
--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -3,58 +3,78 @@ import { VscThreeBars } from "react-icons/vsc";
 
 import { useDispatch } from "react-redux";
 import { logoutThunk } from "../../store/session";
-import { useEffect, useState, useRef} from "react";
+import { useEffect, useState, useRef } from "react";
 import styles from './Navigation.module.css';
+import OpenModalButton from "../OpenModalButton/OpenModalButton";
+import LoginFormModal from "../ LoginFormModal/LoginFormModal"
+import SignupFormModal from "../SignUpFormModal/SignupFormModal";
+import OpenModalMenuItem from "./OpenModalMenuItem";
 
 
-
-const ProfileButton = ({user})=> {
+const ProfileButton = ({ user }) => {
     const dispatch = useDispatch()
     const [showMenu, setShowMenu] = useState(false)
     const ulRef = useRef();
 
-    const handleLogout = (e) => {
-        e.preventDefault()
-        dispatch(logoutThunk())
-    }
-
     const toggleMenu = (e) => {
         e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
         setShowMenu(!showMenu);
-      };
+    };
 
-    useEffect(()=>{
+    useEffect(() => {
         if (!showMenu) return;
 
-        const closeMenu = (e)=> {
-            if (ulRef.current && !ulRef.current.contains(e.target)) {
+        const closeMenu = (e) => {
+            if (!ulRef.current.contains(e.target)) {
                 setShowMenu(false);
-             }
-        }
-      
+            }
+        };
+
         document.addEventListener('click', closeMenu)
         return () => document.removeEventListener('click', closeMenu);
-    },[showMenu])
+    }, [showMenu])
 
+    const closeMenu = () => setShowMenu(false);
+
+
+    const handleLogout = (e) => {
+        e.preventDefault()
+        dispatch(logoutThunk())
+        closeMenu()
+    }
 
     return (
 
         <>
-  
             <button className={styles.profileButton} onClick={toggleMenu}>
-                <VscThreeBars size={'2em'}/>
-                <FaUserCircle size={'2em'}/>
+                <VscThreeBars size={'2em'} />
+                <FaUserCircle size={'2em'} />
             </button>
-            <ul className={showMenu ? styles.profileDropdown: styles.hidden} ref={ulRef}>
-                <li>{user.username}</li>
-                <li>{user.firstName} {user.lastName}</li>
-                <li>{user.email}</li>
-                <li>
-                    <button onClick={handleLogout}>Log Out</button>
-                </li>
+            <ul className={showMenu ? styles.profileDropdown : styles.hidden} ref={ulRef}>
+                {user ? (
+                    <>
+                        <li>{user.username}</li>
+                        <li>{user.firstName} {user.lastName}</li>
+                        <li>{user.email}</li>
+                        <li>
+                            <button onClick={handleLogout}>Log Out</button>
+                        </li>
+                    </>
+                ) : (
+                    <>
+                        <OpenModalMenuItem  // do not nest under an li
+                            itemText="Log In"
+                            modalComponent={<LoginFormModal />}
+                        />
+                        <OpenModalMenuItem
+                            itemText="Sign Up"
+                            modalComponent={<SignupFormModal />}
+                        />
+                    </>
+                )}
             </ul>
 
-    
+
         </>
 
     )
